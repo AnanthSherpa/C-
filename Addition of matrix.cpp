60d82cf1451f6dcd42494fcdc6b27bded5e7a7f1
AnanthSherpa <98215670+AnanthSherpa@users.noreply.github.com>
2022-01-27 18:26:44 +0530

    Create Addition of matrix.cpp

---
+++ Addition of matrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int arr[3][3]={1,2,3,4,5,6,7,8,9};
+   int item[3][3];
+   int sum[3][3];
+   cout<<"Enter the elements of your array"<<endl;
+   for(int i=0;i<3;i++)
+   {
+   for(int j=0;j<3;j++)
+   {
+   cin>>item[i][j];
+   }
+   }
+    for(int i=0;i<3;i++)
+   {
+   for(int j=0;j<3;j++)
+   {
+   sum[i][j]=arr[i][j]+item[i][j];
+   cout<<"The number present in the SUM array in the index ["<<i<<"]["<<j<<"] is "<<sum[i][j]<<endl;
+   
+   }
+   }
+    return 0;
+}
