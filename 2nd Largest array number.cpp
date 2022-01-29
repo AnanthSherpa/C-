db8bfa5da05b68aa6984b08faf9f28d3f125c50d
AnanthSherpa <98215670+AnanthSherpa@users.noreply.github.com>
2022-01-29 18:00:57 +0530

    Create 2nd Largest array number.cpp

---
+++ 2nd Largest array number.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int item[5];int max,maxt,min,mint,ind;
+    for(int i=1;i<=5;i++)
+    {
+        cout<<"enter your "<<i<<"th element"<<endl;
+        cin>>item[i];
+       
+    }
+    for(int j=1;j<=5;j++)
+    {
+        if (item[1]<item[j])
+        {
+        max=item[j];
+        }
+       }
+   
+    for(int j=1;j<=5;j++)
+    {
+        if (item[1]<item[j]&&item[j]!=max)
+        {
+       maxt=item[j]; ;
+        }}
+           cout<<"2nd largest number is "<<maxt;
+    
+    return 0;
+}
