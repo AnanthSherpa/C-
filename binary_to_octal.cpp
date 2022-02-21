d2607c6846c030a271173f190f116fbb7d364c68
AnanthSherpa <98215670+AnanthSherpa@users.noreply.github.com>
2022-02-21 11:26:10 +0530

    Create binary_to_octal.cpp

---
+++ binary_to_octal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   long int binarynum, octalnum = 0, j = 1, remainder;
+  
+  cout<<"Enter any Binary Number: ";
+  cin>>binarynum;
+  while (binarynum != 0)
+   {
+     remainder = binarynum % 10;
+     octalnum = octalnum + remainder * j;
+     j = j * 2;
+     binarynum = binarynum / 10;
+   }
+   cout<<"Equivalent octal value: " <<octalnum;
+    return 0;
+}
